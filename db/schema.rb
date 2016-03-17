8bf7ae6288c7cff7332eedf84ace2616f54781ac
markcadag <markcadag@gmail.com>
2016-03-17 17:59:30 +0800

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316082809) do
+ActiveRecord::Schema.define(version: 20160317051811) do
 
   create_table "expense_types", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -137,6 +137,8 @@ ActiveRecord::Schema.define(version: 20160316082809) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
