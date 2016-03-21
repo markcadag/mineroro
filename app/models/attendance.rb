e5c37f587a1a7b25836431d7940a9256af4a861d
markcadag <markcadag@gmail.com>
2016-03-21 13:20:50 +0800

    add attendance wip

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,16 @@
 class Attendance < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :miner
+  	
+  	belongs_to :user
+  	belongs_to :miner
+
+	def self.update_or_create(attributes)
+	  	assign_or_new(attributes).save
+	end
+
+	def self.assign_or_new(attributes)
+		obj = first || new
+		obj.assign_attributes(attributes)
+		obj
+	end
+
 end
