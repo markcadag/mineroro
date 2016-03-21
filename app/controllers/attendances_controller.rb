b987d414d1143db42d008107622b1636e4d38e0d
markcadag <markcadag@gmail.com>
2016-03-21 09:34:07 +0800

    add miner lsit

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,6 +4,7 @@ class AttendancesController < ApplicationController
   def index
     @attendace = Attendance.where(mine_id: current_mine.id).order('created_at DESC').by_month
     @expense_requests = Expense.where(mine_id: current_mine.id).order('created_at DESC').by_month
+    @miners = Miner.where(mine_id: current_mine.id)
     gon.watch.expenses = @expenses
     @tunnels = @current_mine.tunnels;
     respond_to do |format|
