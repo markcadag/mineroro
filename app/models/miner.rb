e6ec7746a79c2109d0909855658d1c1dbd3b103f
markcadag <markcadag@gmail.com>
2016-03-22 06:00:43 +0800

    add check attendance

--- app/models/miner.rb
+++ app/models/miner.rb
@@ -1,5 +1,9 @@
 class Miner < ActiveRecord::Base
+	self.per_page = 10
 	belongs_to :mine
 	has_one :tunnel
-	has_many :attendances
+
+	has_many :check_attendance
+	has_many :attendances, :through => :check_attendance
+
 end
