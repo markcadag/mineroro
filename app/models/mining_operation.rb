a0f9277c7d910b42cc12cfe064411dad5f9bf8e6
markcadag <markcadag@gmail.com>
2016-03-21 13:22:43 +0800

    add today method

--- app/models/mining_operation.rb
+++ app/models/mining_operation.rb
@@ -1,2 +1,10 @@
 class MiningOperation < ActiveRecord::Base
+  
+  has_many :mining_operation_cost
+  has_many :expenses, :through => :mining_operation_cost  
+
+  def self.today
+    where("created_at >= ?", Time.zone.now.beginning_of_day)
+  end
+  
 end
