b3fb8ab25b1c97d59f0764d0780ecb42e2f4a582
markcadag <markcadag@gmail.com>
2016-03-22 13:02:54 +0800

    add date helper method

--- app/assets/javascripts/script_helper.js
+++ app/assets/javascripts/script_helper.js
@@ -2,3 +2,10 @@ function numberWithCommas(n) {
     var parts=n.toString().split(".");
     return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : "");
 }
+
+/**
+    formate date to MM/dd/yyyy
+**/
+function formatDate(date) {
+   return (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear()
+}
