16f323ce9fdfd9f90dc8f3c6c75c4fae0b399093
markcadag <markcadag@gmail.com>
2016-03-26 08:20:55 +0800

    fix attendance create

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -17,27 +17,25 @@ class AttendancesController < ApplicationController
   def generate_attendance
     date =  Time.strptime(params[:attendance_date], '%m/%d/%Y').strftime('%Y-%m-%d')
     if !Attendance.exists?(attendance_date: date)
-        @attendance = Attendance.new
-        @attendance.mine_id = current_mine.id
-        @attendance.tunnel_id = params[:tunnel_id]
-        @attendance.attendance_date = Time.strptime(params[:attendance_date], '%m/%d/%Y').strftime('%Y-%m-%d')
-        @attendance.miners = Miner.where(mine_id: current_mine.id)
-        respond_to do |format|
-          if @attendance.save
-              format.html { redirect_to :action => "index", :attendance_date => params[:attendance_date]}
-              format.json { render json: @attendance.as_json() }
-          else
-              format.html { render :new }
-              format.json { render json: @attendance.errors, status: :unprocessable_entity }
-          end
+      @attendance = Attendance.new
+      @attendance.mine_id = current_mine.id
+      @attendance.tunnel_id = params[:tunnel_id]
+      @attendance.attendance_date = Time.strptime(params[:attendance_date], '%m/%d/%Y').strftime('%Y-%m-%d')
+      @attendance.miners = Miner.where(mine_id: current_mine.id)
+      respond_to do |format|
+        if @attendance.save
+            format.html { redirect_to :action => "index", :attendance_date => params[:attendance_date]}
+            format.json { render json: @attendance.as_json() }
+        else
+            format.html { render :new }
+            format.json { render json: @attendance.errors, status: :unprocessable_entity }
+        end
         end
     else
-
-    respond_to do |format|
-        format.html { redirect_to :action => "index", :attendance_date => params[:attendance_date]}
-        format.json { head :ok }
-    end
-
+      respond_to do |format|
+          format.html { redirect_to :action => "index", :attendance_date => params[:attendance_date]}
+          format.json { head :ok }
+      end
     end
   end
 
@@ -49,7 +47,12 @@ class AttendancesController < ApplicationController
 
   def create_or_find_attendace
     date = Time.strptime(params[:attendance_date], '%m/%d/%Y').strftime('%Y-%m-%d')
-    @attendance = Attendance.find_or_create_by(mine_id: current_mine.id, attendance_date: date)
+    @attendance = Attendance.find_or_initialize_by(mine_id: current_mine.id, attendance_date: date)
+    if @attendance.new_record?
+       @attendance.miners = Miner.where(mine_id: current_mine.id)
+       @attendance.save!
+    end
+    # @attendance = Attendance.find_or_create_by(mine_id: current_mine.id, attendance_date: date)
   end
 
 
