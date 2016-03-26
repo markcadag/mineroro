d1ee47e3fb882762a85be06e3ac85264cf3ff98e
markcadag <markcadag@gmail.com>
2016-03-26 08:21:14 +0800

    add dashboard data

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def index
+  	@total_sacks_today = TunnelOperation.where(mine_id: current_mine.id, operation_type: 'site').by_day.sum :production_count
+  	@total_fund_req_plant_today = Expense.where(mine_id: current_mine.id, status: 'pending', category: 'plant').by_day.count
+  	@total_fund_req_site_today = Expense.where(mine_id: current_mine.id, status: 'pending', category: 'site').by_day.count
+  	@total_yield = TunnelOperation.where(mine_id: current_mine.id, operation_type: 'plant').by_day.sum :production_count
   end
 end
