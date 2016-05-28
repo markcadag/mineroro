adfacea63660029681399dc02b121c516e188a18
mark <markcadag@gmail.com>
2016-05-29 07:01:55 +0800

    add pagination

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,12 @@ class Item < ActiveRecord::Base
   has_one :inventory
 
   before_create :generate_token
+  self.per_page = 10
 
   protected
 
   def generate_token
-    self.token = loop do
+    self.item_code = loop do
       random_token = SecureRandom.urlsafe_base64(nil, false).upcase
       break random_token unless Item.exists?(item_code: random_token)
     end
