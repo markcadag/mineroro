8969fa1f3fa6ba5cd1e430b24693b0bf48475252
mark cadag <markcadag@gmail.com>
2016-07-07 20:04:11 +0800

    new item logic

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -88,6 +88,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:name, :description, :quantity, :current_amount)
+      params.require(:item).permit(:item_code, :name, :description)
     end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,16 +2,16 @@ class Item < ActiveRecord::Base
   belongs_to :mine
   has_one :inventory
 
-  before_create :generate_token
+  # before_create :generate_token
   self.per_page = 10
 
-  protected
+  # protected
 
-  def generate_token
-    self.item_code = loop do
-      random_token = SecureRandom.urlsafe_base64(nil, false).upcase
-      break random_token unless Item.exists?(item_code: random_token)
-    end
-  end
+  # def generate_token
+  #   self.item_code = loop do
+  #     random_token = SecureRandom.urlsafe_base64(nil, false).upcase
+  #     break random_token unless Item.exists?(item_code: random_token)
+  #   end
+  # end
 
 end
