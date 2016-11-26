5ce45894701264347efee4184d495a918491391b
mark cadag <markcadag@gmail.com>
2016-11-26 11:44:21 +0800

    add tunnel and team

--- app/models/tunnel.rb
+++ app/models/tunnel.rb
@@ -1,8 +1,7 @@
 class Tunnel < ActiveRecord::Base
   belongs_to :mine
   belongs_to :expense
-  has_one :miner
-
+  has_one :miner, :dependent => :destroy
   has_many :tunnel_expenses
   has_many :expenses, :through => :tunnel_expenses
 
