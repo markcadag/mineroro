38da21d110d65a3ff32b41e5f68b1bee54c1f377
mark cadag <markcadag@gmail.com>
2016-12-13 09:30:54 +0800

    payroll blind fix

--- app/models/payroll.rb
+++ app/models/payroll.rb
@@ -2,8 +2,8 @@ class Payroll < ActiveRecord::Base
 	
 	enum payroll_type: {staff_personnel: 1, plant_workers: 2 , km_15: 3, plant_utility: 4, plant: 5, office_personnel: 6, security: 7  }
 
-	has_many :payroll_datums
-	has_many :employees, through: :payroll_datums
+	has_many :payroll_datums , dependent: :destroy
+	has_many :employees, through: :payroll_datums, dependent: :destroy
 
 	accepts_nested_attributes_for :payroll_datums, allow_destroy: true
 
